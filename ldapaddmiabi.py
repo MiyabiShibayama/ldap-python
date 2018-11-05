b78dff94ea755aacb10c2a59ff52e1e11054de6a
miyabi <miyabi@osstech.co.jp>
2018-11-05 13:32:48 +0900

    ldapaddmiyabiの編集

--- ldapaddmiabi.py
+++ ldapaddmiabi.py
@@ -11,23 +11,17 @@ BIND_DN = 'cn=master,dc=example'
 BASE_DN = 'dc=example,dc=jp'
 BIND_PASS = 'secret123'
 
-def main():
-    print('call main')
-    miyabiadd = ldapaadmiyabi('ldap://ldap.exmaple.jp','cn=miyabi2,dc=example','secret123',1001,'Test6'):
-        print('追加されました')
 
 def ldapaddmiyabi(uri,bind_dn,bind_pass,gidNumber,description):
     ld = LDAP(URI)
     ld.bind(BIND_DN,BIND_PASS)
     result = ld.add('cn=miyabi2,ou=Users,dc=example,dc=jp',
-            [('objectClass',['top','person','inetOrgPerson','posixAccount']),
+            [('objectClass',['top','posixAccount']),
              ('cn',['miyabi2']),
              ('gidNumber',['1001']),
              ('description',['Test6'])
                ])
     return result
 
-print('add')
-
-main()
+print('追加されました')
 
